0eb5148733826e9362c2d679a4b39d6154840058
Rana2324 <shohelranatonmoy@gmail.com>
2023-05-09 08:39:13 +0900

    screen share and dynamic video audio select

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const muteBtn = document.getElementById("muteBtn");
 const cameraOff = document.getElementById("cameraoff");
 const selectCam = document.getElementById("selectCam");
 const selectMic = document.getElementById("selectMic");
+const screenShare = document.getElementById("screenShare");
 
 //
 
 let mediaStream;
 let mute = false;
 let camera = true;
+let currentCam;
 
 //mute button
 muteBtn.addEventListener("click", (e) => {
@@ -47,20 +49,66 @@ cameraOff.addEventListener("click", () => {
 });
 
 //get media
-async function getUserMedia() {
+async function getUserMedia(cameraId, micId) {
+  currentCam = cameraId === null ? currentCam : cameraId;
+
+  currentCam = cameraId;
+
+  const initialConfiguration = {
+    video: true,
+    audio: true,
+  };
+  const userPreferredCameraConfiguration = {
+    audio: true,
+    video: {
+      deviceId: cameraId,
+    },
+  };
+  const videoOption = currentCam ? { deviceId: currentCam } : true;
+  const userPreferredMicConfiguration = {
+    video: videoOption,
+    audio: {
+      deviceId: micId,
+    },
+  };
+
+  console.log(currentCam, videoOption);
   try {
-    mediaStream = await window.navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: true,
-    });
+    mediaStream = await window.navigator.mediaDevices.getUserMedia(
+      cameraId || micId
+        ? cameraId
+          ? userPreferredCameraConfiguration
+          : userPreferredMicConfiguration
+        : initialConfiguration
+    );
 
     displayMedia();
+    getAllCameras();
+    getAllMic();
+
+    console.log(mediaStream.getAudioTracks());
   } catch (error) {
     console.log(error);
   }
 }
 
 getUserMedia();
+
+//share screen
+
+async function getShareScreen() {
+  try {
+    mediaStream = await window.navigator.mediaDevices.getDisplayMedia({
+      audio: true,
+      video: true,
+    });
+    displayMedia();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+screenShare.addEventListener("click", getShareScreen);
 
 //display media
 
@@ -71,3 +119,49 @@ function displayMedia() {
     video.play();
   });
 }
+
+//get all camera access
+
+async function getAllCameras() {
+  const currentCamera = mediaStream.getVideoTracks()[0];
+  const allDevices = await window.navigator.mediaDevices.enumerateDevices();
+  selectCam.innerHTML = "";
+  allDevices.forEach((device) => {
+    if (device.kind === "videoinput") {
+      const option = document.createElement("option");
+      option.value = device.deviceId;
+      option.textContent = device.label;
+      option.selected = device.label === currentCamera.label ? true : false;
+      selectCam.appendChild(option);
+    }
+  });
+}
+//get all microphone access
+
+async function getAllMic() {
+  const currentMic = mediaStream.getVideoTracks()[0];
+  const allDevices = await window.navigator.mediaDevices.enumerateDevices();
+  selectMic.innerHTML = "";
+  allDevices.forEach((device) => {
+    if (device.kind === "audioinput") {
+      const option = document.createElement("option");
+      option.value = device.deviceId;
+      option.textContent = device.label;
+      option.selected = device.label === currentMic.label ? true : false;
+      selectMic.appendChild(option);
+    }
+  });
+}
+
+//select a specific camera
+
+selectCam.addEventListener("input", (e) => {
+  const micId = e.target.value;
+  getUserMedia(micId);
+});
+//select a specific micro phone
+
+selectCam.addEventListener("input", (e) => {
+  const cameraId = e.target.value;
+  getUserMedia(currentCam, cameraId);
+});
